a5758d2461570fb6babf011fded7335c5e20d71e
Jari1980 <jari.tenhunen@outlook.com>
2026-09-06 10:56:15 +0200

    initial backend

---
+++ backend/src/main/java/com/device/esp32_protocol_lab/Esp32ProtocolLabApplication.java
@@ -0,0 +1,13 @@
+package com.device.esp32_protocol_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esp32ProtocolLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esp32ProtocolLabApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/device/esp32_protocol_lab/Esp32ProtocolLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.device.esp32_protocol_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esp32ProtocolLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
